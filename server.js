0e3737ec3d6c0572f97001046e0b89cdac6bb836
Alisson Lima <amlxd5@gmail.com>
2022-06-03 16:48:45 -0500

    alteração no server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');  
-const path = require('path');  
-  
-const ngApp = express();  
-  
-ngApp.use(express.static('./dist'));  
-  
-ngApp.get('/*', function (request, response) {  
-    response.sendFile(path.join(__dirname, '/dist/index.html'));  
-});  
-  
-ngApp.listen(process.env.PORT || 4200);
+const express = require('express');
+const path = require('path');
+const nomeApp = process.env.npm_package_name;
+const app = express();
+
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+});
+
+app.listen(process.env.PORT || 4200);
